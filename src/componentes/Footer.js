96024f254946d9dd67c2a7a39286b44a1519d8ed
Cecilia Gerlero <mcgerlero@gmail.com>
2024-07-02 23:38:10 -0300

    fin primer clase reactj

---
+++ src/componentes/Footer.js
@@ -0,0 +1,10 @@
+const Footer = (props) => {
+    return (
+        <footer>
+            <p> Diseñado por MCG</p>
+        </footer>
+        
+    )
+}
+
+export default Footer;
---
+++ src/componentes/Header.js
@@ -0,0 +1,15 @@
+
+
+const Header = (props) => {
+    return (
+
+        <header>
+            <h1>Aprendemos REACT</h1>
+        </header>
+
+
+
+    )
+}
+
+export default Header;
