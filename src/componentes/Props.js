41c31056380479f7da6eed7a2db2d4f4af38f79e
Cecilia Gerlero <mcgerlero@gmail.com>
2024-07-15 20:00:46 -0300

    clase 24

--- src/componentes/Props.js
+++ src/componentes/Props.js
@@ -4,6 +4,17 @@ return(
 )
 }
 
+const EjemploProps2= props =>{
+    
+    const{elementos}= props;
+
+    return(
+        <ul>{elementos.map(eLemento =><li key={eLemento}> {eLemento}</li>)}</ul>
+    )
+    }
+
+
 export{
-    EjemploProps1
+    EjemploProps1,
+    EjemploProps2
 }
--- src/pages/Ejemplo1.js
+++ src/pages/Ejemplo1.js
@@ -1,7 +1,24 @@
+import { EjemploProps1, EjemploProps2 } from "../componentes/Props";
+
+const nombre2 = "Lucas";
+
+const frutas = ['Manzanas', 'Bananas', 'Naranjas', 'Paltas']
+
+const equipos = ['Argentna', 'Colombia', 'Uruguay', 'Canada']
 const Ejemplo1 = (props) => {
     return (
 
-        <p>hola soy el ejemplo1!!</p>
+        <div>
+            <h1>Ejemplos de propiedades</h1>
+            { /* con esto aparece ele HOLA */}
+            <EjemploProps1 nombre="MCG" />
+            <EjemploProps1 nombre={nombre2} />
+
+            {/* listar elempleo del props2 */}
+            <EjemploProps2 elementos={frutas} />
+            <EjemploProps2 elementos={equipos} />
+        </div>
+
 
     )
 }
