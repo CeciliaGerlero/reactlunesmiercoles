94f1b00c1fa4c02f6d0c68c96bd179931c34f3ae
Cecilia Gerlero <mcgerlero@gmail.com>
2024-07-22 09:52:26 -0300

    fin de todo

--- src/pages/Ejemplo5.js
+++ src/pages/Ejemplo5.js
@@ -1,62 +1,61 @@
-import '../styles/ejemplo5.css'
+import '../styles/ejemplo5.css';
+import { useEffect, useState } from 'react';
 
 const DatosApi = (props) => {
 
+    const [cargando, setCargando] = useState(false);
+    const [personajes, setPersonajes] = useState([]);
+    useEffect(() => {
 
+        const cargarDatos = async () => {
+            setCargando(true);
+            const res = await (await fetch('https://rickandmortyapi.com/api/character')).json();
+
+
+            console.group(res)
+            setPersonajes(res.results);
+            setCargando(false);
+
+
+        }
+
+        //fetch: para poder traer datos nativos de JS y otros ej pueden ver q tb utiliza axos(peticioness)
+        cargarDatos();
+
+    }, []);
 
     return (
 
         <>
 
             <h1>Carga de datos a través de una API</h1>
-            <div className="personajes">
-                <div className="personaje">
-                    <h4> Nombre PERSONAJE</h4>
-                    <div className="ficha">
-                        <div className="foto">
-                            <img src="https://fakeimg.pl/150x100" alt="" />
 
+            {cargando ? <p>Cargando ....</p> : (
+                <div className="personajes">
+
+                    {personajes.map(personaje => (
+
+                        <div className="personaje" key={personaje.id}>
+                            <h3>{personaje.id}</h3>
+                            <h4> {personaje.name}</h4>
+                            <div className="ficha">
+                                <div className="foto">
+                                    <img src={personaje.image} alt="{personje.name}" />
+                                </div>
+                                <div className="datos">
+                                    {/* <h6>{personaje.species}</h6> */}
+                                    <h6>Especie: {personaje.species === 'Human' ? 'Humano' : 'Extraterrestre'}</h6>
+
+                                    {/* <h5>{personaje.status}</h5> */}
+                                    <h5>Estado:{personaje.status === 'Alive' ? 'Vivo' : 'Muerto' }</h5>
+                                </div>
+                            </div>
                         </div>
-                        <div className="datos">
-                            <h6>Especie : HUMANO o NO</h6>
-                            <h5>Vivo : SI o NO</h5>
-                        </div>
-                    </div>
+                    ))}
+
                 </div>
+            )}
 
-            </div>
-            <div className="personajes">
-                <div className="personaje">
-                    <h4> Nombre PERSONAJE</h4>
-                    <div className="ficha">
-                        <div className="foto">
-                            <img src="https://fakeimg.pl/150x100" alt="" />
-
-                        </div>
-                        <div className="datos">
-                            <h6>Especie : HUMANO o NO</h6>
-                            <h5>Vivo : SI o NO</h5>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-            <div className="personajes">
-                <div className="personaje">
-                    <h4> Nombre PERSONAJE</h4>
-                    <div className="ficha">
-                        <div className="foto">
-                            <img src="https://fakeimg.pl/150x100" alt="" />
-
-                        </div>
-                        <div className="datos">
-                            <h6>Especie : HUMANO o NO</h6>
-                            <h5>Vivo : SI o NO</h5>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
         </>
     )
 
