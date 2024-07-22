49a7a322b405c01d2d031eeb3acab2a242b4995b
Cecilia Gerlero <mcgerlero@gmail.com>
2024-07-22 10:04:46 -0300

    correccion

--- src/pages/Ejemplo3.js
+++ src/pages/Ejemplo3.js
@@ -5,7 +5,7 @@ const Ejemplo3 = (props) => {
         <>
             <div className="mapa">
                 <h1>Ejemplo google maps - CSS</h1>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3397.7381117716877!2d-60.67761962531757!3d-31.613635105812858!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b5a9f89d965a95%3A0xe75d07537517e4eb!2sAntonia%20Godoy%206384%2C%20S3004%20IWJ%2C%20Santa%20Fe!5e0!3m2!1ses!2sar!4v1721144050612!5m2!1ses!2sar" style={{ width: 850, height: 400, border: 0 }}></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3397.7381117716877!2d-60.67761962531757!3d-31.613635105812858!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b5a9f89d965a95%3A0xe75d07537517e4eb!2sAntonia%20Godoy%206384%2C%20S3004%20IWJ%2C%20Santa%20Fe!5e0!3m2!1ses!2sar!4v1721144050612!5m2!1ses!2sar" style={{ width: 850, height: 400, border: 0 }} title='mapa'></iframe>
 
             </div>
 
